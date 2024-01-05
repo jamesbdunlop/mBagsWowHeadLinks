97c7ae370d4c9e0438119ae446a5f93c4a7a48af
James Dunlop <kryptick@iinet.net.au>
2024-01-05 11:18:02 +1100

    Opps now using a default wow font instead of a mRadial one

--- mBagsWowHeadLinks.lua
+++ mBagsWowHeadLinks.lua
@@ -304,8 +304,8 @@ function mBagsWowHeadLinks:BagPane()
                             -- Note this doesn't work for all icons interActiveIcon:SetImage(C_Item.GetItemIconByID(icon))
                             interActiveIcon:SetImage(MWArtTexturePaths[icon])
                             interActiveIcon:SetImageSize(iconSize, iconSize)
-                            local font = "Interface\\AddOns\\mRadial\\fonts\\DejaVuSansMono.ttf"
-                            interActiveIcon.label:SetFont(font,  iconSize/6, "OUTLINE, MONOCHROME")
+                            local font = "Fonts\\FRIZQT__.TTF"
+                            interActiveIcon.label:SetFont(font,  iconSize/4, "OUTLINE, MONOCHROME")
 
                             interActiveIcon:SetUserData("hyperlink", hyperlink)
                             interActiveIcon:SetUserData("url", url)
